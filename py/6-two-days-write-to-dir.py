1fc0661cea1ed6c11e635a02740a45c4de58aadf
konradp <kpisarczyk@gmail.com>
2020-02-06 20:40:46 +0000

    Get last 2 days

--- py/6-two-days-write-to-dir.py
+++ py/6-two-days-write-to-dir.py
@@ -1,11 +1,28 @@
+#!/usr/bin/env python3
 # Run this as:
 # python 3-save-as-json.py > data.json
 import argparse
 import ib_web_api
 import json
+import os
+from datetime import datetime
 from ib_web_api import MarketDataApi
 from ib_web_api import ContractApi
 from ib_web_api.rest import ApiException
+
+# Settings
+data_dir = 'data-6'
+
+
+# Helpers
+def create_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+
+# Main
+# Create data dir
+create_dir(data_dir)
 
 # Parse args
 parser = argparse.ArgumentParser(
@@ -37,12 +54,26 @@ try:
     # Get market data
     for symbol, conid in conids.items():
         api = MarketDataApi(ib_web_api.ApiClient(config))
-        response = api.iserver_marketdata_history_get(conid, '1d').data
+        response = api.iserver_marketdata_history_get(conid, '2d').data
         datapoints = []
+        # Convert to dict
         for datapoint in response:
             datapoints.append(datapoint.to_dict())
-        f = open('data-5/' + symbol + '.json', 'w')
-        f.write(json.dumps(datapoints))
-        f.close()
+        # Split into two days by date
+        dates = {}
+        for datapoint in datapoints:
+            cur_date = datetime \
+                .fromtimestamp(int(datapoint['t']/1000)) \
+                .strftime('%Y-%m-%d')
+            if cur_date not in dates:
+                dates[cur_date] = []
+            dates[cur_date].append(datapoint)
+        # Write data to dirs
+        for date in dates:
+            date_dir = data_dir + '/' + date
+            create_dir(date_dir)
+            f = open(date_dir + '/' + symbol + '.json', 'w')
+            f.write(json.dumps(dates[date]))
+            f.close()
 except ApiException as e:
     print("Exception: %s\n" % e)
