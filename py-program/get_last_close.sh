2f2cb3707096e72df6a94960d2f17db2bf205f48
konradp <kpisarczyk@gmail.com>
2020-03-08 00:28:09 +0000

    Add get_last_close.sh

---
+++ py-program/get_last_close.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Filter to get only symbols which are in IB and get conids
+IN_FILE=data_sh/nasdaq_symbols
+OUT_FILE=data_sh/nasdaq_symbols_ib
+if [[ -f $OUT_FILE ]]; then
+  rm $OUT_FILE
+fi
+touch $OUT_FILE
+
+while read I; do
+  CONID=$(./get_conid_nasdaq.py $I &>/dev/null)
+  if [[ $? == 0 ]]; then
+    echo $I $CONID
+    echo $I $CONID >> $OUT_FILE
+  fi
+done < $IN_FILE
