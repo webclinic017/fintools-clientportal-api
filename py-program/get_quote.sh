aaa93ba2a5d65df5b9b25d844b26b41c5a04ff14
konradp <kpisarczyk@gmail.com>
2020-03-08 00:30:13 +0000

    Remove debug

--- py-program/get_quote.sh
+++ py-program/get_quote.sh
@@ -4,13 +4,11 @@
 # ./get_quote 209011562
 
 [[ -z $1 ]] && echo No conid && exit 1
-
 CONID=$1
 
 URL="https://localhost:5000\
 /v1/portal/iserver/marketdata/history\
-?conid=209011562\
+?conid=$CONID\
 &period=3d&bar=1d"
-echo $URL
 curl -k -X GET $URL \
   -H 'Content-Type: application/x-www-form-urlencoded'
