ac234b401b9c78bb3d9e9d73c1e15e510bd8dc7b
konradp <kpisarczyk@gmail.com>
2020-03-10 20:56:58 +0000

    Update tickle script with keepalive

--- cron/tickle.sh
+++ cron/tickle.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-curl -sk "https://localhost:5000/v1/portal/tickle"
+API=https://localhost:5000/v1/portal
+curl -sk "$API/tickle" >/dev/null
+curl -sk "$API/sso/validate" >/dev/null
+AUTHED=$(curl -sk "$API/iserver/auth/status" | jq -r '.authenticated')
+if [[ $AUTHED == 'false' ]]; then
+  curl -sk "$API/iserver/reauthenticate"
+  echo -n
+fi
