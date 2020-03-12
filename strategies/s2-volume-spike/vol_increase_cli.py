33fe4c5d842e44e63d798ae73e15e1ff8c285740
konradp <kpisarczyk@gmail.com>
2020-03-12 20:49:55 +0000

    Parse arguments on strategy 2

--- strategies/s2-volume-spike/vol_increase_cli.py
+++ strategies/s2-volume-spike/vol_increase_cli.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 # Run strategy from command line
+import argparse
 from vol_increase import VolIncrease
 
-a = VolIncrease()
-a.run([ 'AAPL', 'AMZN'])
+# Parse args
+parser = argparse.ArgumentParser(description='Run strategy 2.')
+parser.add_argument(
+  'symbols',
+  metavar='SYMBOLS',
+  type=str,
+  nargs='+',
+  help='Symbols'
+)
+args = parser.parse_args()
+symbols = args.symbols
+
+# Run strategy
+strategy = VolIncrease()
+strategy.run(symbols)
