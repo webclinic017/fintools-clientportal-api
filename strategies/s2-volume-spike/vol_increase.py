7b4f3fc17baf123cc6e764bbed7382ecdd1acff3
konradp <kpisarczyk@gmail.com>
2020-03-12 17:54:31 +0000

    Add boilerplate for strategy 2 vol increase

---
+++ strategies/s2-volume-spike/vol_increase.py
@@ -0,0 +1,5 @@
+class VolIncrease:
+  # Given a list of tickers, return tickers which spiked X% volume within last
+  # Y time, e.g. 15% increase from the lowest volume over last 15min.
+  def run(self, symbols):
+    print('Running on symbols: ' + ' '.join(symbols))
---
+++ strategies/s2-volume-spike/vol_increase_cli.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Run strategy from command line
+from vol_increase import VolIncrease
+
+a = VolIncrease()
+a.run([ 'AAPL', 'AMZN'])
