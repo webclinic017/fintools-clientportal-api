6c5f42b052f69e85ecf89127bd0925340b1e08e4
konradp <kpisarczyk@gmail.com>
2020-03-16 23:05:50 +0000

    Find conid from symbol

--- strategies/s2-volume-spike/vol_increase.py
+++ strategies/s2-volume-spike/vol_increase.py
@@ -1,4 +1,5 @@
 import ib_web_api
+from lib.company import Company
 from datetime import datetime
 from ib_web_api import MarketDataApi
 from ib_web_api.rest import ApiException
@@ -8,33 +9,31 @@ class VolIncrease:
   # Given a list of tickers, return tickers which spiked X% volume within last
   # Y time, e.g. 15% increase from the lowest volume over last 15min.
   def run(self, symbols):
+    symbols = [i.upper() for i in symbols]
     # Instantiate API class
     config = ib_web_api.Configuration()
     config.verify_ssl = False
     client = ib_web_api.ApiClient(config)
     api = MarketDataApi(client)
-    # TODO: Use utility method here: translate symbols to conids
-    symbols = { 'AAPL': '265598', 'AMZN': '3691937' }
-    for symbol, conid in symbols.items():
+    for symbol in symbols:
+      print('===> ' + symbol)
       # Get last 15 minutes data
+      # TODO: Catch here
       points = api.iserver_marketdata_history_get(
-        conid,
+        Company(symbol).get_conid(),
         '15min',
         bar='1min'
       ).data
-      # Convert to dict
-      data = [ p.to_dict() for p in points ]
       # Get just volume
-      vol = [i['v'] for i in data]
-      # Min/max volume points
+      vol = [ p.to_dict()['v'] for p in points ]
+      # Min/max/mean volume points
       lo = min(i for i in vol if i > 0)
       hi = max(vol)
       m = mean(vol)
-      # Perc increase from median
+      # Perc increase from mean
       perc = ((hi-m)/m)*100
-      print('SYMBOL: ' + symbol + '(' + conid + ')')
-      print('CONID: ' + conid)
       print('LO: ' + str(lo))
       print('HI: ' + str(hi))
       print('ME: ' + str(round(m, 2)))
       print('PERC: ' + str(round(perc, 2)) + '%')
+      print('CONID: ' + Company(symbol).get_conid())
