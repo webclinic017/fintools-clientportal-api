d55a4461086b84e9e6bf7c3515965fceb5ec4046
Konrad Pisarczyk <konrad.pisarczyk@godaddy.com>
2020-04-19 23:44:56 +0100

    Add perc_show_graph symbols_test_over_time

--- strategies/s4-backtest/perc_show_graph.py
+++ strategies/s4-backtest/perc_show_graph.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3.7
 # Run strategy from command line
 # It uses python3.7 because otherwise json.dumps doesn't preserve dict order
+# This doesn't really do anything, other than tshowing how to display
+# charts
+import matplotlib.pyplot as plt
+
 import argparse
-import datetime
 import ib_web_api
 import json
-import matplotlib.dates as mdates
-import matplotlib.pyplot as plot
 import pprint
 import test_data
 import urllib3
@@ -31,15 +32,9 @@ parser.add_argument(
   action='store_true',
   help='If enabled, get real data from IB'
 )
-parser.add_argument(
-  '-d', '--dates-only',
-  action='store_true',
-  help='Get dates for strategies, and chart them',
-)
 args = parser.parse_args()
 symbols = args.symbols
 test = args.test
-dates_only = args.dates_only
 
 ##### HELPERS
 
@@ -99,11 +94,7 @@ def get_price_plus_perc(price, perc):
 def get_count_from_price(buy_price, data):
   # Given 'prices' range, { 'buy': 1, 'sell': 2 }
   # gives count of how often able to buy and sell at given prices
-  # also gives dates
-  ret = {
-    'count': 0,
-    'dates': [],
-  }
+  count = 0
   # Sort data by timestamp
   data = sorted(data, key=lambda k: k['t'])
   if buy_price is None:
@@ -117,9 +108,8 @@ def get_count_from_price(buy_price, data):
     if not find_buy:
       if point['h'] >= get_price_plus_perc(buy_price, 4):
         find_buy = True
-        ret['count'] += 1
-        ret['dates'].append(timestamp_to_date(point['t']))
-  return ret
+        count += 1
+  return count
 
 ###### MAIN
 if test:
@@ -142,13 +132,6 @@ for i in data:
 del data
 
 # Run each strategy
-strategies = [
-  'close',
-  'low',
-  'perc_avg',
-  'perc_first',
-  'perc_low',
-]
 ret = {
   'summary': {
     'success_days': 0,
@@ -158,62 +141,19 @@ ret = {
 }
 
 # Get count of successful buy/sell pairs (benchmark)
+avges = []
+print('LENGTH:', len(data_by_date))
 for date, points in sorted(data_by_date.items()):
-  success_count = len(get_perc_from_history(points, 4))
-  if success_count > 0:
-    ret['summary']['success_days'] += 1
-  ret['summary']['success_total'] += success_count
+  #d = get_perc_from_history(points, 4)
+  #if len(d) > 0:
+  a = str_perc_avg(points)
+  if a is not None:
+    avges.append(str_perc_avg(points))
+  else:
+    avges.append(0)
+pprint.pprint(avges)
 
-for strategy in strategies:
-  strategy_info = {
-    'success_total': 0,
-    'success_days': 0,
-    'success_dates': [],
-    'success_days_perc': 0,
-  }
-  print('====', strategy)
-  prev = None
-  for date, points in sorted(data_by_date.items()):
-    if prev is not None:
-      print(date, ':', round(prev, 2), 'and',
-        round(get_price_plus_perc(prev, 4), 2)
-      )
-      day_count = get_count_from_price(prev, points)
-      strategy_info['success_total'] += day_count['count']
-      if day_count['count'] > 0:
-        strategy_info['success_days'] += 1
-        strategy_info['success_dates'] += day_count['dates']
-    else:
-      print('No prev data')
-      pass
-    prev = get_buy_price(strategy, points)
-  # Work out percentage success
-  a = strategy_info['success_days']
-  b = ret['summary']['success_days']
-  strategy_info['success_dates'] = sorted(list(set(strategy_info['success_dates'])))
-  strategy_info['success_days_perc'] = round((a/b)*100, 2)
-  ret['summary']['strategies'][strategy] = strategy_info
-# Sort strategies by percentage
-strats = ret['summary']['strategies']
-ret['summary']['strategies'] = {
-  k: v for k,v in sorted(
-    strats.items(),
-    key=lambda item: item[1]['success_days_perc']
-  )
-}
-
-# Print
-if dates_only:
-  formatter = mdates.DateFormatter('%Y-%m-%d')
-  axes = plot.gca() # Get axes
-  axes.xaxis.set_major_formatter(formatter)
-  strategy_idx = 0
-  for strategy in ret['summary']['strategies']:
-    strategy_idx += 1
-    dates = ret['summary']['strategies'][strategy]['success_dates']
-    print(strategy, dates)
-    dates = [datetime.datetime.strptime(d,'%Y%m%d').date() for d in dates]
-    plot.scatter(dates, [ strategy_idx for i in range(len(dates))])
-  plot.show()
-else:
-  print(json.dumps(ret, indent=2))
+pprint.pprint(ret)
+plt.plot(avges)
+plt.ylabel('some numbers')
+plt.show()
