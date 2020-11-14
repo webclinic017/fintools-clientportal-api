094d28cc2bd6be64bf29401f4cf451a5ce909064
Konrad Pisarczyk <konrad.pisarczyk@godaddy.com>
2020-11-14 17:37:27 +0000

    Add connect timeout to tickl

--- cron/tickle.sh
+++ cron/tickle.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 API=https://localhost:5000/v1/portal
-curl -sk "$API/tickle" >/dev/null
-curl -sk "$API/sso/validate" >/dev/null
-AUTHED=$(curl -sk "$API/iserver/auth/status" | jq -r '.authenticated')
-if [[ $AUTHED == 'false' ]]; then
-  curl -sk "$API/iserver/reauthenticate"
+CURL="curl -sk --connect-timeout 2"
+$CURL "$API/tickle" >/dev/null
+$CURL "$API/sso/validate" >/dev/null
+AUTHED=$($CURL "$API/iserver/auth/status" | jq -r '.authenticated')
+if [[ $AUTHED == 'false' || $AUTHED == '' ]]; then
+  echo Reauthenticating
+  $CURL "$API/iserver/reauthenticate"
   echo -n
 fi
