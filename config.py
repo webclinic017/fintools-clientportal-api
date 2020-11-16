822cf99f0eb0d51d7b993eda0625221a6d467d7c
Konrad Pisarczyk <konrad.pisarczyk@godaddy.com>
2020-11-16 01:32:30 +0000

    Update url_cheap_symbols to localhost

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 dir_conids = '/opt/fintools-ib/data/conids'
 dir_day = '/opt/fintools-ib/data/day'
 dir_quote = '/opt/fintools-ib/data/quotes'
-url_cheap_symbols = 'http://5.152.176.191/lt/3'
+url_cheap_symbols = 'http://localhost/lt/3'
 url_nasdaq_list = 'ftp://ftp.nasdaqtrader.com/symboldirectory/nasdaqlisted.txt'
