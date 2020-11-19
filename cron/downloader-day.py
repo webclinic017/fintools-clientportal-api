da5f74d2eab2b5b7847308a8edfb59b79c1e1f2b
Konrad Pisarczyk <konrad.pisarczyk@godaddy.com>
2020-11-19 22:30:44 +0000

    Add exception handling for downloader-day.py

--- cron/downloader-day.py
+++ cron/downloader-day.py
@@ -55,36 +55,41 @@ parser.add_argument('symbols',
 )
 args = parser.parse_args()
 
-# Get cheap symbols
-with urllib.request.urlopen(config.url_cheap_symbols) as response:
-  symbols = json.loads(response.read().decode('utf-8'))
-  day_quotes = {}
-  if debug is True:
-    symbols = { key: symbols[key] for key in list(symbols)[0:5] }
-  if args.symbols:
-    symbols = args.symbols
-  count_total = len(symbols)
-  with concurrent.futures.ThreadPoolExecutor(max_workers=400) as executor:
-    future_to_data = {
-      executor.submit(get_quote, symbol): symbol
-      for symbol in symbols
-    }
-    for future in concurrent.futures.as_completed(future_to_data):
-      try:
-        day_quotes.update(future.result())
-      except Exception as e:
-        # Failed to get conid, skip
-        print('EXC: %s' % e)
-        pass
-  # Save to day dir
-  print('Got %i quotes' % len(day_quotes))
-  for f in glob.glob(config.dir_day + '/*.json'):
-    os.remove(f)
-  for symbol in day_quotes:
-    f_path = config.dir_day + '/' + symbol + '.json'
-    with open(f_path, 'w') as f:
-      if day_quotes[symbol] is not None:
-        f.write(json.dumps(day_quotes[symbol]))
-      else:
-        print(f'No data for {symbol}')
-        os.remove(f_path)
+try:
+  # Get cheap symbols
+  with urllib.request.urlopen(config.url_cheap_symbols) as response:
+    symbols = json.loads(response.read().decode('utf-8'))
+    day_quotes = {}
+    if debug is True:
+      symbols = { key: symbols[key] for key in list(symbols)[0:5] }
+    if args.symbols:
+      symbols = args.symbols
+    count_total = len(symbols)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=400) as executor:
+      future_to_data = {
+        executor.submit(get_quote, symbol): symbol
+        for symbol in symbols
+      }
+      for future in concurrent.futures.as_completed(future_to_data):
+        try:
+          day_quotes.update(future.result())
+        except Exception as e:
+          # Failed to get conid, skip
+          print('EXC: %s' % e)
+          pass
+    # Save to day dir
+    print('Got %i quotes' % len(day_quotes))
+    for f in glob.glob(config.dir_day + '/*.json'):
+      os.remove(f)
+    for symbol in day_quotes:
+      f_path = config.dir_day + '/' + symbol + '.json'
+      with open(f_path, 'w') as f:
+        if day_quotes[symbol] is not None:
+          f.write(json.dumps(day_quotes[symbol]))
+        else:
+          print(f'No data for {symbol}')
+          os.remove(f_path)
+except urllib.error.URLError as e:
+  print('Failed getting %s: %s %s' % (e.url, e.code, e.msg))
+except Exception as e:
+  print('Failed getting cheap symbols')
