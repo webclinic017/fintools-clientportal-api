08f3832637454e269fc8d256c1b87f831bbbce6b
Konrad Pisarczyk <konrad.pisarczyk@godaddy.com>
2020-12-01 01:19:32 +0000

    Remove debug

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ dir_quote = '/opt/fintools-ib/data/quotes'
 url_cheap_symbols = 'http://localhost/lt/3'
 url_nasdaq_list = 'ftp://ftp.nasdaqtrader.com/symboldirectory/nasdaqlisted.txt'
 # For test
-debug = True
+debug = False
 downloader_limit = '10'
