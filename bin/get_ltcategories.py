1e84475f3982a8a986166ab6f0c809d94ac851f7
Konrad Pisarczyk <konrad.pisarczyk@godaddy.com>
2020-12-04 00:03:38 +0000

    Format out as json

--- bin/get_ltcategories.py
+++ bin/get_ltcategories.py
@@ -11,7 +11,7 @@ from lib.filters import get_contracts_cheaper_than
 # Main
 try:
   contracts = get_contracts_cheaper_than(3, redownload=True)
-  print(contracts)
+  print(json.dumps(contracts))
 except Exception as e:
   print('ERROR: Could not get contracts:', e)
   exit(1)
